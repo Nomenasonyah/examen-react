d12e566fa281ccb0350963a9a2e1b5de397881b5
nomenasonyah <nomena.sonyah@gmail.com>
2025-02-13 11:41:54 +0300

    change directory img

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar: React.FC = () => {
       <img src="./src/img/logo.png" alt="logo" />
     <nav>
       <ul className="list_nav" style={{ display: "flex", gap: "20px", listStyle: "none" ,flexDirection:"column",padding:"0px"}}>
-        <li className="li_nav"><img src="./src/img/dash.png" alt="" /><Link to="/dashboard" className="link_style">Dashboard</Link></li>
-        <li className="li_nav"><img src="./src/img/star.png" alt="" /><Link to="/products" className="link_style">Products</Link></li>
-        <li className="li_nav"><img src="./src/img/user.png" alt="" /><Link to="/clients" className="link_style">Clients</Link></li>
-        <li className="li_nav"><img src="./src/img/store.png" alt="" /><Link to="/store" className="link_style">Store</Link></li>
+        <li className="li_nav"><img src="./dash.png" alt="" /><Link to="/dashboard" className="link_style">Dashboard</Link></li>
+        <li className="li_nav"><img src="./star.png" alt="" /><Link to="/products" className="link_style">Products</Link></li>
+        <li className="li_nav"><img src="./user.png" alt="" /><Link to="/clients" className="link_style">Clients</Link></li>
+        <li className="li_nav"><img src="./store.png" alt="" /><Link to="/store" className="link_style">Store</Link></li>
       </ul>
     </nav>
     </div>
